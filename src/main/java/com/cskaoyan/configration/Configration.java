b6f8d74734040cdea35b1d93297bc59cc440343c
caoshuo <cs_youcanbelieve@163.com>
2019-05-17 11:32:07 +0800

    提交人:caoshuo 功能:上传pom和注解配置application id:119

---
+++ src/main/java/com/cskaoyan/configration/Configration.java
@@ -0,0 +1,9 @@
+package com.cskaoyan.configration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+@ComponentScan(basePackages = "com.cskaoyan")
+@Configuration
+public class Configration {
+
+}
